82c04a67b675ef7a30cab739c5aaf22a7cfe9d6b
Liz Yu <qqqq929@hotmail.com>
2023-11-24 01:19:46 +0800

    fix deploy issue in github page

--- src/containers/Routes/MainRouter.tsx
+++ src/containers/Routes/MainRouter.tsx
@@ -2,13 +2,21 @@ import { createBrowserRouter } from 'react-router-dom';
 import Category from '../Category';
 import Main from '../Main';
 
-export const MainRouter = createBrowserRouter([
+// BrowserRoute is not allowed in github page deploying
+// basename is use for tricky way to use BrowserRoute when deploy to github page
+// @see https://create-react-app.dev/docs/deployment/#github-pages
+export const MainRouter = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Main />,
+    },
+    {
+      path: 'category',
+      element: <Category />,
+    },
+  ],
   {
-    path: '/',
-    element: <Main />,
-  },
-  {
-    path: 'category',
-    element: <Category />,
-  },
-]);
+    basename: '/ingrid-portfolio',
+  }
+);
